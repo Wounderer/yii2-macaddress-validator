2b27d7cb4e27f85a0ce80c81e50701a5ee900b67
Vladimir Korovin <rolan1986@gmail.com>
2015-03-06 22:08:29 +0300

    Hello, world!

---
+++ composer.js
@@ -0,0 +1,23 @@
+{
+    "name": "vakorovin/yii2-macaddress-validator",
+    "keywords": ["yii", "validator", "macaddress"],
+    "description": "Yii2 MAC-address validator",
+    "homepage": "https://github.com/vakorovin/yii2-macaddress-validator",
+    "license": "MIT",
+    "type": "yii2-extension",
+	"minimum-stability":"stable",
+    "authors": [
+        {
+            "name": "Vladimir",
+            "email": "rolan1986@gmail.com",
+            "homepage": "https://github.com/vakorovin",
+            "role": "Developer"
+        }
+    ],
+    "autoload": {
+        "psr-4": {"vakorovin\\yii2_macaddress_validator\\": ""}
+    },
+    "require": {
+        "yiisoft/yii2": "*"
+    }
+}
